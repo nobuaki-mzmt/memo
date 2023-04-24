10a8ccebaf95c31b1697374d247ff9f49cc622a4
Nobuaki Mizumoto (水元 惟暁) <71577022+nobuaki-mzmt@users.noreply.github.com>
2023-04-24 16:13:57 +0900

    Create raspvery.bat

---
+++ raspvery.bat
@@ -0,0 +1,12 @@
+# copy file from raspbery pi to windows
+# run the following in cmd
+scp pi@10.9.193.108:/var/www/html/media/*.jpg G:/rpi/01
+scp pi@10.9.193.225:/var/www/html/media/*.jpg G:/rpi/02
+scp pi@10.9.193.37:/var/www/html/media/*.jpg G:/rpi/03
+scp pi@10.9.193.190:/var/www/html/media/*.jpg G:/rpi/04
+scp pi@10.9.193.163:/var/www/html/media/*.jpg G:/rpi/05
+scp pi@10.9.193.48:/var/www/html/media/*.jpg G:/rpi/06
+scp pi@10.9.193.232:/var/www/html/media/*.jpg G:/rpi/07
+scp pi@10.9.193.39:/var/www/html/media/*.jpg G:/rpi/08
+scp pi@10.9.193.81:/var/www/html/media/*.jpg G:/rpi/09
+scp pi@10.9.193.171:/var/www/html/media/*.jpg G:/rpi/10

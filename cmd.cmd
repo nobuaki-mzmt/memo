99e0ba0335c55ed5962b1525f06de1c9eeb6d8b2
Nobuaki Mizumoto (水元 惟暁) <71577022+nobuaki-mzmt@users.noreply.github.com>
2023-08-28 10:36:36 +0900

    Update cmd.cmd

--- cmd.cmd
+++ cmd.cmd
@@ -34,8 +34,11 @@ ffmpeg -ss 20 -i input.mp4 -t3600 output.mp4
 for f in *.MP4; do ffmpeg -ss 20 -i "$f" -t 3600 -c:v h264_nvenc -b:v 1000k -s 1920x1080 %%i-small.mp4; done
 :: Windows
 for /r %f in (*.mp4) do ffmpeg -ss 20 -i "$f" -t 3600 -c:v h264_nvenc -b:v 1000k -s 1920x1080 %%i-small.mp4
+for /r %f in (*.wmv) do ffmpeg -i "%~f" -c:v h264_nvenc "%~nf.mp4"
 :: When running a for loop directly in cmd, need to use % instead of %% as the loop variable.
 :: This is because the %% symbol is used to escape variables in batch files, but is not necessary when running a loop directly in cmd.
+
+
 
 :: ---- speed change ----
 ffmpeg -i input.mp4 -vf setpts=PTS/3.0 -af atempo=2.0 outputx2.mp4
